075f09735c8efc75e85b4263bfffaa90bebc576a
yingyang14 <YING.YANG14@qmail.cuny.edu>
2024-12-10 00:41:18 +0000

    added final

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,8 +1,14 @@
 export default function Page() {
-    return (
-      <>
-      <h1 className="text-3xl">Final Exam</h1>
-      </>
-    );
-  }
+  return (
+    <>
+    <h1 className="text-3xl">Final Exam</h1>
+    <form>
+      <label htmlFor="product">Product Description</label>
+      <input type="text" name="product" id="product" className="text-black bg-yellow-400"/>
+      <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit">Save</button>
+    </form>
+    </>
+  );
+}
+
   
