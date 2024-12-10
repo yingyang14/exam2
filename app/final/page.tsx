637136059dbd4575aa30be44b1de893988be7933
yingyang14 <YING.YANG14@qmail.cuny.edu>
2024-12-10 00:58:22 +0000

    style form

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,12 +1,13 @@
 export default function Page() {
   return (
     <>
-    <h1 className="text-3xl">Final Exam</h1>
+    <h1 className="text-3xl ml-5">Final Exam</h1>
     <form>
-      <label htmlFor="product">Product Description</label>
-      <input type="text" name="product" id="product" className="text-black bg-yellow-400"/>
-      <button className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800" type="submit">Save</button>
-    </form>
+      <label htmlFor="product"className="ml-5">Product Description</label>
+      <input type="text" name="product" id="product" className="m-5 text-black bg-yellow-400"/>
+      <br />
+      <button className="ml-10 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700  dark:focus:ring-green-800" type="submit">Save</button>
+       </form>
     </>
   );
 }
