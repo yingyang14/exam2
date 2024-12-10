cc3b29cf6c1707ad31f743af75db63fa256c1eba
yingyang14 <YING.YANG14@qmail.cuny.edu>
2024-12-10 00:03:43 +0000

    added date

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 export default function Home() {
   return (
     <>
+    Date: 12/9/2024
       <h1 className="m-8 text-blue-600"> Our Story</h1>
       <p className="mx-20 my-9">Pursuing beauty to the perfect extend will never go out of date. We Care about our customers! Your 100% satisfaction is our ultimate goal and we are aimed to deliver you the state-of-art customer services and after sale cares. 
 
